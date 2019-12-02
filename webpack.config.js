601c622f5385c991ee35775c02a5282cc9b48125
cn-wx <xuwang2@student.unimelb.edu.au>
2019-12-03 07:58:33 +0800

    chore: enable minimize in html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -69,10 +69,11 @@ module.exports = {
       },
       {
         test: /\.html$/,
-        loaders: [
+        use: [
           {
-            loader: 'html-loader',
-          }
+            loader: "html-loader",
+            options: { minimize: true },
+          },
         ],
       },
       {
