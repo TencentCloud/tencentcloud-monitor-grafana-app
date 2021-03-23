03bfbefc3c972833eff6b1d74c456a7871e0d193
Cloudlie <cloudlieluo@gmail.com>
2021-03-23 16:07:15 +0800

    fix: 云函数监控数据源支持使用变量来获取实例信息

--- src/datasource/tc_monitor/scf/datasource.ts
+++ src/datasource/tc_monitor/scf/datasource.ts
@@ -1,6 +1,7 @@
 import * as _ from 'lodash';
-import { GetServiceAPIInfo } from '../../common/constants';
+import { FilterKeys, GetServiceAPIInfo, SliceLength } from '../../common/constants';
 import { BaseDatasource } from '../_base/datasource';
+import { MetricQuery } from '../_base/types';
 import { SCFInstanceAliasList, SCFInvalidDemensions } from './query_def';
 
 export default class SCFDatasource extends BaseDatasource {
@@ -48,6 +49,101 @@ export default class SCFDatasource extends BaseDatasource {
       { region, action: 'ListVersionByFunction' },
     ).then(response => {
       return response.Versions.map(({ Version }) => ({ text: Version, value: Version }));
+    });
+  }
+
+  async metricFindQuery(query: MetricQuery, regex?: string) {
+    // 查询地域列表
+    const { action, namespace, display, filterkey, filtervalue } = query;
+    let { region, instancealias = this.templateQueryIdMap.instance } = query;
+
+    if (!action || !namespace) {
+      return [];
+    }
+
+    const variableQuery = {};
+    if (filterkey && FilterKeys.indexOf(filterkey) !== -1) {
+      variableQuery[filterkey] = filtervalue;
+    }
+
+    // 查询地域列表
+    const regionQuery = action.match(/^DescribeRegions$/i);
+    if (regionQuery) {
+      return this.getRegions();
+    }
+
+    region = this.getVariable(region); // 将模板转换为真实值
+
+    // 查询实例列表
+    if (region && action.match(/^DescribeInstances/i)) {
+      const result = await this.getVariableInstances(region, variableQuery);
+      // 缓存全量实例列表
+      this.instanceListCache = result;
+
+      instancealias = this.InstanceAliasList.includes(instancealias) ? instancealias : this.templateQueryIdMap.instance;
+      return result.flatMap(item => {
+        item._InstanceAliasValue = item[instancealias]; // FIXME:
+        if (!item[instancealias]) return [];
+        return [
+          {
+            text: this.formatVarDisplay(item, display, instancealias),
+            value: item[this.templateQueryIdMap.instance],
+          },
+        ];
+      });
+    }
+
+    // 在instance实例的基础上查询其他数据
+    let instance = this.getVariable(query['instance']);
+    if (region && action && instance) {
+      try {
+        // instance = JSON.parse(instance);
+        instance =
+          _.cloneDeep(this.instanceListCache.find(item => item[this.templateQueryIdMap.instance] === instance)) ?? {};
+        // eslint-disable-next-line no-empty
+      } catch (error) {}
+      return this.fetchMetricData(action, region, instance, query);
+    }
+
+    return Promise.resolve([]);
+  }
+
+  getVariableInstances(region, query = {}): Promise<any[]> {
+    let result: any[] = [];
+    const params = { ...{ Offset: 0, Limit: 100 }, ...query };
+
+    const { service = this.service, action, responseField: field } = this.InstanceReqConfig;
+
+    const serviceInfo = GetServiceAPIInfo(region, service);
+    return this.doRequest(
+      {
+        url: this.url + serviceInfo.path,
+        data: params,
+      },
+      serviceInfo.service,
+      { region, action },
+    ).then(response => {
+      result = _.get(response, field) ?? _.get(response, `Result.${field}`) ?? [];
+      const total = response.TotalCount || 0;
+      if (result.length >= total) {
+        return result;
+      } else {
+        const param = SliceLength(total, 100);
+        const promises: any[] = [];
+        _.forEach(param, item => {
+          promises.push(this.getInstances(region, { ...item, ...query }));
+        });
+        return Promise.all(promises)
+          .then(responses => {
+            _.forEach(responses, item => {
+              result = _.concat(result, item);
+            });
+            return result;
+          })
+          .catch(error => {
+            return result;
+          });
+      }
     });
   }
 }
