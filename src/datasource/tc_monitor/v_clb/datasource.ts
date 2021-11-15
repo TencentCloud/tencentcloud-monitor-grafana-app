61150f1a4e71f415b2148d2fa8d2ae051535c11c
James Wang <36892657+jamesxwang@users.noreply.github.com>
2021-11-15 17:23:05 +0800

    feat: optimize CLB namespace and dashboard (#76)
    * feat: 新增v_clb产品；redis（5s）预设面板；ckafka问题修复等
    
    * fix: disable `autoEnabled`
    
    * build(release): 2.3.0
    
    Co-authored-by: leonlysu <leonlysu@tencent.com>

---
+++ src/datasource/tc_monitor/v_clb/datasource.ts
@@ -0,0 +1,62 @@
+import {
+  VCLBInstanceAliasList,
+  VCLBInvalidDemensions,
+  namespace,
+  templateQueryIdMap,
+  clbNamespaceMap,
+  regionSupported,
+  modifyDimensons,
+} from './query_def';
+import { BaseDatasource } from '../_base/datasource';
+import _ from 'lodash';
+import { GetServiceAPIInfo } from '../../common/constants';
+
+export default class DCDatasource extends BaseDatasource {
+  Namespace = namespace;
+  InstanceAliasList = VCLBInstanceAliasList;
+  InvalidDimensions = VCLBInvalidDemensions;
+  templateQueryIdMap = templateQueryIdMap;
+  // 此处service是接口的配置参数，需和plugin.json里一致，和constant.ts中SERVICES_API_INFO保持一致
+  InstanceReqConfig = {
+    service: 'clb',
+    action: 'DescribeLoadBalancers',
+    responseField: 'LoadBalancerSet',
+  };
+  constructor(instanceSettings, backendSrv, templateSrv) {
+    super(instanceSettings, backendSrv, templateSrv);
+  }
+
+  getRegions() {
+    return Promise.resolve(regionSupported);
+  }
+  async getAllMetrics(region: string) {
+    const serviceInfo = GetServiceAPIInfo(region, 'monitor');
+    const { MetricSet: public_metrics } = await this.doRequest(
+      {
+        url: this.url + serviceInfo.path,
+        data: {
+          Namespace: clbNamespaceMap.public,
+        },
+      },
+      serviceInfo.service,
+      { region, action: 'DescribeBaseMetrics' }
+    );
+    const { MetricSet: private_metrics } = await this.doRequest(
+      {
+        url: this.url + serviceInfo.path,
+        data: {
+          Namespace: clbNamespaceMap.private,
+        },
+      },
+      serviceInfo.service,
+      { region, action: 'DescribeBaseMetrics' }
+    );
+    console.log({ private_metrics, public_metrics });
+    const res = Object.values(_.keyBy(_.concat(private_metrics, public_metrics), 'MetricName'));
+    return res.map((t) => modifyDimensons(t));
+  }
+  async getMetrics(region = 'ap-guangzhou') {
+    const rawSet = await this.getAllMetrics(region);
+    return _.compact(rawSet);
+  }
+}
