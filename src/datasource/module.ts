5decd54031979bb0ce5bf1c46db164729a5ff7ee
James Wang <36892657+jamesxwang@users.noreply.github.com>
2022-02-25 11:41:58 +0800

    fix: remove exposure of getBackendSrv and getTemplateSrv (#100)
    * fix: remove exposure of getBackendSrv and getTemplateSrv
    
    * build: remove exposure of getBackendSrv and getTemplateSrv

--- src/datasource/module.ts
+++ src/datasource/module.ts
@@ -3,16 +3,8 @@ import { QueryEditor } from './QueryEditor';
 import { VariableQueryEditor } from './VariableQueryEditor';
 import { DataSource } from './DataSource';
 import { ConfigEditor } from './config-editor/ConfigEditor';
-import { IS_DEVELOPMENT_ENVIRONMENT } from './common/constants';
-import { getBackendSrv, getTemplateSrv } from '@grafana/runtime';
 
 export const plugin = new DataSourcePlugin<any>(DataSource)
   .setQueryEditor(QueryEditor)
   .setConfigEditor(ConfigEditor)
   .setVariableQueryEditor(VariableQueryEditor);
-
-if (IS_DEVELOPMENT_ENVIRONMENT) {
-  (window as any).tcPlugin = plugin;
-  (window as any).tcBackendSvr = getBackendSrv();
-  (window as any).tcTemplateSrv = getTemplateSrv();
-}
